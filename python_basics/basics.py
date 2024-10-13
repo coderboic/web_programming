513f787ed18150368d95364b866156066f0e9425
Rishabh <rsharma4_be23@thapar.edu>
2024-10-13 11:25:21 +0530

    adding python basics file

---
+++ python_basics/basics.py
@@ -0,0 +1,10 @@
+name=input("Name: ");
+print("Hello, " + name);
+print(f"Hello , {name}");
+n=int(input("Number: "))
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
---
+++ python_basics/sequence.py
@@ -0,0 +1,7 @@
+names=["Harry","Ron","Hermione"]
+print(names)
+print(names[0])
+coordinateX=10.0
+coordinateY=20.0
+coordinate=(coordinateX,coordinateY)
+print(coordinate)
